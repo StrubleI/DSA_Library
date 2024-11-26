b30525daae5d749306308372d9cf9fb257a2d5af
StrubleI <100692877+StrubleI@users.noreply.github.com>
2024-11-26 18:49:07 +0100

    Add files via upload

--- Stack.h
+++ Stack.h
@@ -22,6 +22,7 @@ private:
 
 public:
 
+	~Stack();
 	Stack() : top(nullptr), stackSize(0);
 
 	void push(T value); // Add an element to the top
@@ -32,5 +33,63 @@ public:
 	void display() const; // Display the stack contents
 
 };
+
+template <typename T>
+void Stack<T>::push(T value) {
+	Node* newNode = new Node(value);
+	newNode->next = top;
+	top = newNode;
+	stacksize++;
+}
+
+template <typename T>
+Stack<T>::~Stack() {
+	while (!isEmpty){
+		pop();
+	}
+}
+
+template <typename T>
+T Stack<T>::pop() {
+	if (isEmpty()) {
+		throw std::underflow_error("Stack underflow: Cannot pop from an empty stack.");
+	}
+	
+	Node* temp = top;
+	T poppedValue = top->data;
+	top = top->next;
+	delete temp;
+	stacksize--;
+	return poppedValue;
+
+}
+
+template <typename T>
+T Stack<T>::peek() const{
+	if (isEmpty()) {
+		throw std::underflow_error("Stack is empty: can't peek.")
+	}
+	else return top->data;
+}
+
+template <typename T>
+bool Stack<T>::isEmpty() const {
+	return top == nullptr;
+}
+
+template <typename T>
+size_t Stack<T>::size() const {
+	return stackSize;
+}
+
+template <typename T>
+void Stack<T>::display() const {
+	Node* current = top;
+	std::cout << "Stack contents: ";
+	while (current) {
+		std::cout << current->data << " -> ";
+		current->next;
+	}
+}
 
 #endif
